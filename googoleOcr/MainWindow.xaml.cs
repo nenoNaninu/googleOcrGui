03a1d3c4e815dde525aadfab53d159a9e6c801e5
nenoNaninu <neno@neno-garden.com>
2018-04-03 11:02:30 +0900

    とりあえずうごく

--- googoleOcr/MainWindow.xaml.cs
+++ googoleOcr/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +19,41 @@ namespace googoleOcr
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = this;
+
         }
+
+        public int Scale
+        {
+            get { return (int)CanvasScale * 100; }
+            set { CanvasScale = value / 100.0; }
+        }
+
+        private double canvasScale = 1.0;
+        public double CanvasScale
+        {
+            get { return canvasScale; }
+            set
+            {
+                if (value > 0)
+                {
+                    canvasScale = value;
+                }
+                OnPropertyChanged("CanvasScale");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged = (s, e) => { };
+
+        private void OnPropertyChanged(string name)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+
     }
 }
