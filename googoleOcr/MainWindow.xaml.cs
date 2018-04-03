93c93126aaf01005e6f4cbb844f9a06faf7e33ba
nenoNaninu <neno@neno-garden.com>
2018-04-03 17:57:10 +0900

    canvasの大きさちゃんと変更できました

--- googoleOcr/MainWindow.xaml.cs
+++ googoleOcr/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +19,75 @@ namespace googoleOcr
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = this;
+            canvasHeight = (int)ScrollView.ActualHeight;
+            canvasWidth = (int)ScrollView.ActualWidth;
+        }
+
+        int canvasHeight;
+        public int CanvasHeight
+        {
+            get { return canvasHeight; }
+            set
+            {
+                canvasHeight = value;
+                OnPropertyChanged(nameof(CanvasHeight));
+            }
+        }
+
+        int canvasWidth;
+        public int CanvasWidth
+        {
+            get { return canvasWidth; }
+            set
+            {
+                canvasWidth = value;
+                OnPropertyChanged(nameof(CanvasWidth));
+            }
+        }
+
+        int scale = 100;//これはパーセント。
+
+        public int Scale
+        {
+            get
+            {
+                return scale;
+            }
+            set
+            {
+                scale = value;
+                CanvasScale = value;
+                OnPropertyChanged("Scale");
+                CanvasWidth = (int)(CanvasScale * ScrollView.ActualWidth);
+                CanvasHeight = (int)(CanvasScale * ScrollView.ActualHeight);
+            }
+        }
+
+        double canvasScale = 1.0;//少数による倍率。
+
+
+        public double CanvasScale
+        {
+            get
+            { return canvasScale; }
+            set
+            {
+                canvasScale = value / 100f;
+                OnPropertyChanged(nameof(CanvasScale));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string name)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(name));
         }
     }
 }
